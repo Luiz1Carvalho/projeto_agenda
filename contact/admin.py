3338db2c577a7769fa895e9c333ac3d3fbc59ca3
Luiz Carlos <luizcarlosluga@yahoo.com.br>
2026-03-27 15:02:55 -0300

    Initial

---
+++ contact/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from .models import Category, Contact
+"""LUIZ - 123"""
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'created_date')
+    ordering = ('-created_date',)
+    list_filter = ('created_date',)
+    search_fields = ('first_name', 'last_name', 'phone', 'email')
+    list_per_page = 10
+    list_max_show_all = 100
+    #list_editable = ('first_name', 'last_name', 'phone', 'email')
+    list_display_links = ('id', 'first_name',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('-id',)
---
+++ contact/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
