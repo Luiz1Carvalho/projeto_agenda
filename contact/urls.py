5f59d01e73887369bcd021b6cb385d6091086578
Luiz Carlos <luizcarlosluga@yahoo.com.br>
2026-04-02 15:40:29 -0300

    adicionado paginação e algumas outras alterações que esqueci de enviar

--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls.static import static
 app_name = 'contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,11 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from contact.models import Contact
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 def index(request):
     contact = Contact.objects.filter(show=True).order_by('-id')
 
+    paginator = Paginator(contact, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contact': contact,
+        'page_obj': page_obj,
+        'site_title': 'Agenda de Contatos',
+    }
+
+    return render(request, 'contact/index.html', context)
+
+def contact(request, contact_id):
+    single_contact = Contact.objects.get(id=contact_id)
+    context = {
+        'contact': single_contact,
+        'site_title': "Contato - " + single_contact.first_name,
+    }
+
+    return render(request, 'contact/contact.html', context)
+
+def search(request):
+    query = request.GET.get('q')
+    contact = Contact.objects.filter(show=True).filter(
+                                     Q(first_name__icontains=query) |
+                                     Q(last_name__icontains=query) |
+                                     Q(email__icontains=query) |
+                                     Q(phone__icontains=query)
+                                     ).order_by('-id')
+
+    if query is None or query.strip() == '':
+        return redirect('contact:index')
+    
+    paginator = Paginator(contact, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'site_title': 'Procurando por: ' + query,
+        'valor_pesquisa': query,
     }
 
     return render(request, 'contact/index.html', context)
