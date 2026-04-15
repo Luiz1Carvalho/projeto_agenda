a7d3c7344cfbc9dcf06bf22b81260e90955e635b
Luiz Carlos <luizcarlosluga@yahoo.com.br>
2026-04-15 14:49:28 -0300

    adicionado mais funções para CRUD de contatos

--- contact/forms.py
+++ contact/forms.py
@@ -1,14 +1,32 @@
-
 from django import forms
 from contact import models
+from django.core.exceptions import ValidationError
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(widget=forms.FileInput(attrs={'accept': 'image/*'}))
+    
     class Meta:
         model = models.Contact
-        fields = ('first_name', 'last_name', 'phone',)
+        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category', 'picture')
 
         #widgets = {'first_name': forms.PasswordInput()}
 
         widgets = {'first_name': forms.TextInput(
             attrs={'class': 'form-control', 
                    'placeholder': 'Seu nome'})}
+        
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        msg = ValidationError('O nome e sobrenome não podem ser iguais', code='invalid')
+        if first_name ==  last_name:
+            self.add_error('last_name', msg)
+        
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == "ASD":
+            self.add_error('first_name', ValidationError('Não pode ser esse nome', code='invalid'))
+        return first_name
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.forms import ContactForm
+from contact.models import Contact
 
 def create (request):
     if request.method == 'POST':
-        context = {'form': ContactForm(request.POST)}
-        
-        return render(request, 'contact/create.html',context)
+        form = ContactForm(request.POST, request.FILES)
+        context = {'form': form}
+        if form.is_valid():
+            form.save()
+            return redirect('contact:create')
+        return render(request, 'contact/create.html',context, )
 
 
     context = {'form': ContactForm()}
     return render(request, 'contact/create.html', context)
+
+def update(request, contact_id):
+    contact = get_object_or_404(Contact, id=contact_id)
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contact:contact', contact_id=contact.id)
+    else:
+        form = ContactForm(instance=contact)
+    
+    context = {'form': form, 'contact': contact, 'site_title': 'Editar Contato'}
+    return render(request, 'contact/update.html', context)
+
+def delete(request, contact_id):
+    contact = get_object_or_404(Contact, id=contact_id)
+    
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('contact:index')
